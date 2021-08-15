bdd001357ce913667f437e6688a547f69ec65b89
Pavel Matviyuk <pav.matt@mail.ru>
2021-08-16 00:25:48 +0300

    main() was added

---
+++ light_ball_league/src/main.cpp
@@ -0,0 +1,36 @@
+#include <string>
+#include <memory>
+
+#include "season.h"
+#include "league.h"
+
+using namespace std;
+
+int main()
+{
+	// empty window
+
+	// set initial season
+	size_t init_year = 2016; // INPUT
+	time_of_year init_season = time_of_year::SUMMER; // INPUT
+	auto season = make_unique<Season>(init_year, init_season);
+
+	// create tournament
+	auto LBL = make_unique<League>(season);
+	LBL->set();
+
+	// create another
+
+	// play tournament
+	// make shedule
+	// round: auto rand or input
+	
+	// all leagues completed:
+	// add stats
+	// jump season
+
+
+
+
+	return 0;
+}
